1ca9f3e446a252080c92f3e512cb8e269b840aad
Rico Mitchell <ricomitch@icloud.com>
2019-10-25 05:15:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,16 @@
 # Your code goes here!
-class Dog
+class Dog 
   
+  def bark
+    puts "woof!"
+  end
   
-lassie = Dog.new
-lassie.name = "Lassie"
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def name 
+    @this_dogs_name
+  end
+  
 end
-end 
